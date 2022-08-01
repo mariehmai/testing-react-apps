ae11818b290d5b1279b1f9eb1d51e7cf046c5c15
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-08-01 18:14:24 +0100

    test(08-extra3): use renderHooks

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -1,43 +1,32 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
 
-import * as React from 'react'
-import {render, act} from '@testing-library/react'
+import {act, renderHook} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-function setup({initialProps} = {}) {
-  const result = {}
-  function TestComponent() {
-    Object.assign(result, useCounter(initialProps))
-    return null
-  }
-  render(<TestComponent />)
-  return result
-}
-
 test('exposes the count and increment/decrement functions', async () => {
-  const result = setup()
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(1)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+  const {result} = renderHook(useCounter)
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
 
 test('allows customization of the initial count', async () => {
-  const result = setup({initialProps: {initialCount: 10}})
-  expect(result.count).toBe(10)
-  act(() => result.increment())
-  expect(result.count).toBe(11)
-  act(() => result.decrement())
-  expect(result.count).toBe(10)
+  const {result} = renderHook(useCounter, {initialProps: {initialCount: 10}})
+  expect(result.current.count).toBe(10)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(11)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(10)
 })
 
 test('allows customization of the step', async () => {
-  const result = setup({initialProps: {step: 2}})
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(2)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+  const {result} = renderHook(useCounter, {initialProps: {step: 2}})
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
