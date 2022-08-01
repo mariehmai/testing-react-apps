d7b7e8c4c7502f660d9241af73cf7629ad5b232a
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-08-01 13:49:07 +0100

    test(04-extra1): use jest mock

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -7,21 +7,17 @@ import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
 test('submitting the form calls onSubmit with username and password', async () => {
-  let submittedData = {}
-  const handleSubmit = data => (submittedData = data)
-
+  const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
-
   const username = 'the-user'
   const password = 'pass'
 
   await userEvent.type(screen.getByLabelText(/username/i), username)
   await userEvent.type(screen.getByLabelText(/password/i), password)
-
   await userEvent.click(screen.getByRole('button', {name: 'Submit'}))
-
-  expect(submittedData).toEqual({
+  expect(handleSubmit).toHaveBeenCalledWith({
     username,
     password,
   })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
