7b632817f5a5ef6fc51dbccfb36743dc16144be3
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-08-01 13:53:18 +0100

    test(04-extra2): use faker to generate test data

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,13 +4,20 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import faker from 'faker'
 import Login from '../../components/login'
+
+function buildLoginForm() {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', async () => {
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
-  const username = 'the-user'
-  const password = 'pass'
+  const {username, password} = buildLoginForm()
 
   await userEvent.type(screen.getByLabelText(/username/i), username)
   await userEvent.type(screen.getByLabelText(/password/i), password)
